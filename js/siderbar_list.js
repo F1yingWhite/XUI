b1786a7288f5d12a8fd2903e9c20d7d6138781ff
许一涵 <81723334@qq.com>
2024-04-20 12:06:14 +0800

    feat:布局组件+重构

--- js/siderbar_list.js
+++ js/siderbar_list.js
@@ -1,10 +1,24 @@
 var listItems = document.querySelectorAll('.siderbar_list');
 
+var dict = {
+    "Button按钮": "./components/button/index.html",
+    "Layout布局组件": "./components/layout/index.html",
+    "Color颜色": "./components/color/index.html",
+}
+
 for (var i = 0; i < listItems.length; i++) {
     listItems[i].addEventListener('click', (event) => {
         for (var j = 0; j < listItems.length; j++) {
             listItems[j].classList.remove('siderbar_list_selected');
         }
         event.target.classList.add('siderbar_list_selected');
+        var path = dict[event.target.innerText];
+        var main = document.querySelector('main');
+        main.innerHTML = '';
+        var iframe = document.createElement('iframe');
+        iframe.src = path;
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        main.appendChild(iframe);
     });
 }
