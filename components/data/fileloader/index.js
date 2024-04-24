d4221c5e663c886ecef6de1ed6561a029936eeb9
许一涵 <81723334@qq.com>
2024-04-24 15:55:45 +0800

    图片文件上传

--- components/data/fileloader/index.js
+++ components/data/fileloader/index.js
@@ -28,20 +28,41 @@ uploadButtons.forEach(function (button) {
 
 
 
+const imgUploadButtons = document.querySelectorAll(".file-img");
+
+imgUploadButtons.forEach(function (button) {
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.accept = "image/*";//设置文件类型为图片
+    fileInput.style.display = "none";
+    document.body.appendChild(fileInput);
+    fileInput.addEventListener("change", function () {
+        //读取图片并且显示
+        const file = fileInput.files[0];
+        if (file) {
+            const fileName = file.name;
+            const li = document.createElement("li");
+            li.textContent = fileName;
+            li.className = "file-list";
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                const img = document.createElement("img");
+                img.src = e.target.result;
+                img.className = "file-img-list";
+                li.addEventListener("click", function () {
+                    li.remove();
+                    img.remove();
+                });
+                button.parentElement.appendChild(li);
+                button.parentElement.appendChild(img);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+    button.addEventListener("click", function () {
+        //得到文件
+        fileInput.click();
+    });
+});
 
 
-// const reader = new FileReader();
-// reader.onload = function () {
-//     //创建一个img标签
-//     const img = document.createElement("img");
-//     img.src = reader.result;
-//     //创建一个a标签
-//     const a = document.createElement("a");
-//     a.href = reader.result;
-//     a.download = file.name;
-//     a.textContent = file.name;
-//     //将img标签和a标签添加到页面上
-//     document.body.appendChild(img);
-//     document.body.appendChild(a);
-// };
-// reader.readAsDataURL(file);
